260dac80dc8d136c80e1694348f6aceceee63656
Dylan Soungpanya <dsoungpa@ucsc.edu>
2021-04-19 18:22:58 -0700

    Changed to no monkey, just dart

--- src/prefabs/Monkey.js
+++ src/prefabs/Monkey.js
@@ -1,3 +1,4 @@
+// Rocket {player} prefab
 class Rocket extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
@@ -6,7 +7,6 @@ class Rocket extends Phaser.GameObjects.Sprite{
         scene.add.existing(this);
         this.isFiring = false;             // track rocket firing status
         this.moveSpeed = 2;                // pixels per frame
-        this.sfxRocket = scene.sound.add('sfx_rocket');
     }
 
     update(){
@@ -18,22 +18,6 @@ class Rocket extends Phaser.GameObjects.Sprite{
             else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
                 this.x += this.moveSpeed;
             }
-        }
-
-        // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
-            this.isFiring = true;
-            this.sfxRocket.play();
-        }
-
-        // if fired, move the rocket up
-        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
-            this.y -= this.moveSpeed;
-        }
-        
-        // reset on miss
-        if(this.y <= borderUISize * 3 + borderPadding){
-            this.reset();
         }
     }
 
